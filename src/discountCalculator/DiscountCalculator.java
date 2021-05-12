17edb624461a1deb6931588b765ecdb1eb9ad614
Jamille Bittencourt <jamille.bittencourt@thoughtworks.com>
2021-05-12 10:52:34 -0300

    First test

--- src/discountCalculator/DiscountCalculator.java
+++ src/discountCalculator/DiscountCalculator.java
@@ -2,4 +2,10 @@ package discountCalculator;
 
 public class DiscountCalculator {
 
+    public double calculatePurchaseWithDiscount(double purchaseValue) {
+        if(purchaseValue <= 499.99){
+            return purchaseValue;
+        }
+        return 0d;
+    }
 }
--- src/test/discountCalculatorTest.java
+++ src/test/discountCalculatorTest.java
@@ -1,6 +1,31 @@
 package test;
 
+import discountCalculator.DiscountCalculator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class discountCalculatorTest {
+
+    DiscountCalculator discountCalculator;
+
+    @BeforeEach
+    void setUp(){
+        discountCalculator = new DiscountCalculator();
+    }
+
+    @Test
+    void shouldNotGiveDiscountWhenPurchaseIsUntil499(){
+        //given
+        double purchaseValue = 499.99;
+        double expectedValue = 499.99;
+
+        //when
+        double purchaseWithDiscount = discountCalculator.calculatePurchaseWithDiscount(purchaseValue);
+
+        //then
+        Assertions.assertEquals(expectedValue, purchaseWithDiscount);
+    }
 
 
 
