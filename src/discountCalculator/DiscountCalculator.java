976a63686225002b68ce5b8d1035c53846dd2268
Luiz Massa <luimassapontes@gmail.com>
2021-06-29 21:13:01 -0300

    commit with a possible solution of test for discount-calculator-dojo

--- src/discountCalculator/DiscountCalculator.java
+++ src/discountCalculator/DiscountCalculator.java
@@ -2,10 +2,28 @@ package discountCalculator;
 
 public class DiscountCalculator {
 
-    public double calculatePurchaseWithDiscount(double purchaseValue) {
+    public double calculatePurchaseWithDiscount(double purchaseValue) throws Exception {
+
+        if(purchaseValue <= 0.0) {
+            throw new Exception("Invalid Value");
+        }
+
         if(purchaseValue <= 499.99){
-            return purchaseValue;
+            return purchaseValue - calculatePercent(purchaseValue, 5);
+        } else if (purchaseValue >= 500.00 && purchaseValue < 1000.00) {
+            return purchaseValue - calculatePercent(purchaseValue, 10);
+        } else if (purchaseValue >= 1000.00) {
+            return  purchaseValue - calculatePercent(purchaseValue, 20);
         }
         return 0d;
+    }
+
+
+    public double calculatePercent(double value, int percent) throws Exception {
+
+        if(value <= 0.0 || percent <= 0) {
+            throw new Exception("Invalid value");
+        }
+        return (value*percent)/100;
     }
 }
--- src/test/discountCalculatorTest.java
+++ src/test/discountCalculatorTest.java
@@ -15,7 +15,7 @@ public class discountCalculatorTest {
     }
 
     @Test
-    void shouldNotGiveDiscountWhenPurchaseIsUntil499(){
+    void shouldNotGiveDiscountWhenPurchaseIsUntil499() throws Exception {
         //given
         double purchaseValue = 499.99;
         double expectedValue = 499.99;
@@ -26,6 +26,213 @@ public class discountCalculatorTest {
         //then
         Assertions.assertEquals(expectedValue, purchaseWithDiscount);
     }
+
+    @Test
+    void shouldGiveTenPercentDiscountWhenPurchaseStartIn500() throws Exception {
+
+        //given
+        double purchaseValue = 500.00;
+        double expectedValue = 450.00;
+
+        //when
+        double purchaseWithTenPercentDiscount = discountCalculator.calculatePurchaseWithDiscount(purchaseValue);
+
+        //then
+        Assertions.assertEquals(expectedValue, purchaseWithTenPercentDiscount);
+
+    }
+
+    @Test
+    void shouldGiveTenPercentDiscountWhenPurchaseStartMajor500Until1000() throws Exception {
+
+        //given
+        double purchaseValue = 750.00;
+        double expectedValue = 675.00;
+
+        //when
+        double purchaseWithTenPercentDiscount = discountCalculator.calculatePurchaseWithDiscount(purchaseValue);
+
+        //then
+        Assertions.assertEquals(expectedValue, purchaseWithTenPercentDiscount);
+
+    }
+
+    @Test
+    void shouldGiveTwentyPercentDiscountWhenPurchaseStartIn1000() throws Exception {
+
+        //given
+        double purchaseValue = 1000.00;
+        double expectedValue = 800.00;
+
+        //when
+        double purchaseWithTwentyPercentDiscount = discountCalculator.calculatePurchaseWithDiscount(purchaseValue);
+
+        //then
+        Assertions.assertEquals(expectedValue, purchaseWithTwentyPercentDiscount);
+
+    }
+
+    @Test
+    void shouldGiveTwentyPercentDiscountWhenPurchaseStartMajor1300() throws Exception {
+
+        //given
+        double purchaseValue = 1300.00;
+        double expectedValue = 1040.00;
+
+        //when
+        double purchaseWithTwentyPercentDiscount = discountCalculator.calculatePurchaseWithDiscount(purchaseValue);
+
+        //then
+        Assertions.assertEquals(expectedValue, purchaseWithTwentyPercentDiscount);
+
+    }
+
+
+    @Test
+    void shouldGiveFivePercentDiscountWhenPurchaseIsLess500() throws Exception {
+
+        //given
+        double purchaseValue = 100.00;
+        double expectedValue = 95.00;
+
+        //when
+        double purchaseWithFivePercentDiscount = discountCalculator.calculatePurchaseWithDiscount(purchaseValue);
+
+        //then
+        Assertions.assertEquals(expectedValue, purchaseWithFivePercentDiscount);
+
+    }
+
+
+    @Test
+    void shouldThrownExceptionWhenPurchaseValueIsZero() {
+
+        //given
+        double purchaseValue = 0;
+        String expectedMessage = "Invalid Value";
+
+        //when
+        Exception exception = Assertions.assertThrows(Exception.class, () -> {
+            discountCalculator.calculatePurchaseWithDiscount(purchaseValue);
+        });
+        String actualMessage = exception.getMessage();
+
+
+        //then
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
+
+    }
+
+    @Test
+    void shouldThrownExceptionWhenPurchaseValueIsNegative() {
+
+        //given
+        double purchaseValue = -200.50;
+        String expectedMessage = "Invalid Value";
+
+        //when
+        Exception exception = Assertions.assertThrows(Exception.class, () -> {
+            discountCalculator.calculatePurchaseWithDiscount(purchaseValue);
+        });
+        String actualMessage = exception.getMessage();
+
+
+        //then
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
+
+    }
+
+
+    @Test
+    void shouldThrownExceptionWhenPercentIsZero() {
+
+        //given
+        int percent = 0;
+        double value = 100;
+        String expectedMessage = "Invalid value";
+
+        Exception exception = Assertions.assertThrows(Exception.class, () -> {
+            discountCalculator.calculatePercent(value, percent);
+        });
+        String actualMessage = exception.getMessage();
+
+
+        //then
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    void shouldThrownExceptionWhenPercentIsNegative() {
+
+        //given
+        int percent = -15;
+        double value = 100;
+        String expectedMessage = "Invalid value";
+
+        Exception exception = Assertions.assertThrows(Exception.class, () -> {
+            discountCalculator.calculatePercent(value, percent);
+        });
+        String actualMessage = exception.getMessage();
+
+
+        //then
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+
+    @Test
+    void shouldReturn20WhenValueIs100() throws Exception {
+
+        //given
+        int percent = 20;
+        double value = 100;
+        double expectedValue = 20;
+
+        //when
+        double purchaseWithFivePercentDiscount = discountCalculator.calculatePercent(value, percent);
+
+        //then
+        Assertions.assertEquals(expectedValue, purchaseWithFivePercentDiscount);
+
+    }
+
+    @Test
+    void shouldThrownExceptionWhenValueIsZero() {
+
+        //given
+        int percent = 50;
+        double value = 0.0;
+        String expectedMessage = "Invalid value";
+
+        Exception exception = Assertions.assertThrows(Exception.class, () -> {
+            discountCalculator.calculatePercent(value, percent);
+        });
+        String actualMessage = exception.getMessage();
+
+
+        //then
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    void shouldThrownExceptionWhenValueIsNegative() {
+
+        //given
+        int percent = 50;
+        double value = -100;
+        String expectedMessage = "Invalid value";
+
+        Exception exception = Assertions.assertThrows(Exception.class, () -> {
+            discountCalculator.calculatePercent(value, percent);
+        });
+        String actualMessage = exception.getMessage();
+
+
+        //then
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+
 
 
 
